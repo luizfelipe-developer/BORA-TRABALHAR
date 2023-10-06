f1dd0a127e47a1cddd9f88d2feb998bd0977c372
Felipe75-hub <developerfelipe777@gmail.com>
2023-10-06 14:21:44 -0300

    php att
    .

--- componentes/paginas/cadastro/php_cadastro/cadastro_cliente.php
+++ componentes/paginas/cadastro/php_cadastro/cadastro_cliente.php
@@ -6,23 +6,29 @@ $sobrenome = $_POST['sobrenome'];
 $cpf = $_POST['cpf'];
 $dt_nascimento = $_POST['dt_nascimento'];
 $genero = $_POST['genero'];
-$endereco = $_POST['endereco'];
 $cep = $_POST['cep'];
+$uf = $_POST['uf'];
+$cidade = $_POST['cidade'];
+$bairro = $_POST['bairro'];
+$endereco = $_POST['endereco'];
+$numero = $_POST['numero'];
 $telefone = $_POST['telefone'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $confirmacao_senha = $_POST['confirmacao_senha'];
+
 //inserir os valores adicionados das variáveis nos campos da tabela cliente do BD
-$inserirSql = "INSERT INTO cad_cliente(nome, sobrenome, cpf, dt_nascimento, genero, endereco, cep, telefone, email, senha, confirmacao_senha)
- VALUES('$nome', '$sobrenome', '$cpf', '$dt_nascimento', '$genero', '$endereco', '$cep', '$telefone', '$email', '$senha', '$confirmacao_senha')";
-//sempre que os valores forem do tipo varchar, devem ficar entre 'aspas simples' 
+$inserirSql = "INSERT INTO cad_cliente(nome, sobrenome, cpf, dt_nascimento, genero, cep, uf, cidade, bairro, endereco, numero, telefone, email, senha, confirmacao_senha) 
+VALUES ('$nome', '$sobrenome', '$cpf', '$dt_nascimento', '$genero', '$cep' , '$uf', '$cidade','$bairro', '$endereco', '$numero', '$telefone', '$email', '$senha', '$confirmacao_senha')";
+//sempre que os valores forem do tipo varchar, devem ficar entre 'aspas simples'
 //Verificação
 if (mysqli_query($conexao, $inserirSql)) {
     echo "Usuário cadastrado!";
-}else{
+} else {
     echo "Usuário não cadastrado. Erro: ".mysqli_connect_error($conexao);
 }
 //encerrar a conexão, para evitar travamentos no BD
 mysqli_close($conexao);
-header('Location: ../../entrar/logins_form.html');
+header('Location: Location: ../../entrar/logins_form.html')
+
 ?>
--- componentes/paginas/cadastro/php_cadastro/cadastro_colaborador.php
+++ componentes/paginas/cadastro/php_cadastro/cadastro_colaborador.php
@@ -26,3 +26,40 @@ if (mysqli_query($conexao, $inserirSql)) {
 mysqli_close($conexao);
 header('Location: ../../entrar/pglogins.html');
 ?>
+
+
+<?php 
+include("../../php/conexao.php");
+//receber as iformações repassadas pelo método POST pelo formulário
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$cpf = $_POST['cpf'];
+$dt_nascimento = $_POST['dt_nascimento'];
+$genero = $_POST['genero'];
+$profissao = $_POST['profissao'];
+$cep = $_POST['cep'];
+$uf = $_POST['uf'];
+$cidade = $_POST['cidade'];
+$bairro = $_POST['bairro'];
+$endereco = $_POST['endereco'];
+$numero = $_POST['numero'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$confirmacao_senha = $_POST['confirmacao_senha'];
+
+//inserir os valores adicionados das variáveis nos campos da tabela cliente do BD
+$inserirSql = "INSERT INTO cad_cliente(nome, sobrenome, cpf, dt_nascimento, genero, profissao, cep, uf, cidade, bairro, endereco, numero, telefone, email, senha, confirmacao_senha) 
+VALUES ('$nome', '$sobrenome', '$cpf', '$dt_nascimento', '$genero', '$profissao', '$cep' , '$uf', '$cidade','$bairro', '$endereco', '$numero', '$telefone', '$email', '$senha', '$confirmacao_senha')";
+//sempre que os valores forem do tipo varchar, devem ficar entre 'aspas simples'
+//Verificação
+if (mysqli_query($conexao, $inserirSql)) {
+    echo "Usuário cadastrado!";
+} else {
+    echo "Usuário não cadastrado. Erro: ".mysqli_connect_error($conexao);
+}
+//encerrar a conexão, para evitar travamentos no BD
+mysqli_close($conexao);
+header('Location: Location: ../../entrar/logins_form.html')
+
+?>
