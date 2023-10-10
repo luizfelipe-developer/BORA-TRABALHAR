db368c4da0f2a8f34d35bcd30571a66a272d14fd
Felipe-Dev <felipe-developer@outlook.com>
2023-10-10 16:45:08 -0300

    link certo no diarista

--- componentes/paginas/servicos/diarista.php
+++ componentes/paginas/servicos/diarista.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "shortcut icon" type = "imagem/x-icon" href="../../../ico-sem-fundo.ico.ico"/>
     <link rel="stylesheet" href="../../header/header.css">
-    <link rel="stylesheet" href="../../css/servico.css">
+    <link rel="stylesheet" href="../../css/servicocopy.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="./../../js/script.js" defer></script>
